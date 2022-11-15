0edd9a6039ef87e596e6b6c9bbcb459e5704affd
Wusoye <baptiste.vrignaud85@gmail.com>
2022-11-15 11:45:52 +0100

    #1

---
+++ src/Controller/MainController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home() {
+        $series = [[
+            'name' => 'Infinite',
+            'sortie' => date('2022-09-10'),
+            'type' => 'ScieFy',
+            'duree' => 154
+        ],[
+            'name' => 'Interstellar',
+            'sortie' => date('2017-06-18'),
+            'type' => 'ScieFy',
+            'duree' => 210
+        ],[
+            'name' => 'Le jour J',
+            'sortie' => date('2025-12-25'),
+            'type' => 'Docu.',
+            'duree' => 65
+        ]];
+        return $this->render('main/home.html.twig', [
+            'series' => $series
+        ]);
+    }
+
+    #[Route("/test", name: "main_test")]
+    public function test() {
+        return $this->render('main/test.html.twig');
+    }
+
+}
